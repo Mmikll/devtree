85fb35b987a965671d61c2458b6b858f5dc9674a
Miguel <mmirandasanchez16@gmail.com>
2025-02-25 16:28:52 -0300

    try

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,12 +1,12 @@
 import { CorsOptions } from "cors";
 export const corsConfig: CorsOptions = {
-  origin: function (origin, handler) {
+  origin: function (origi, handler) {
     const whiteList = [process.env.FRONTEND_URL];
 
     if (process.argv[2] === "--api") {
       whiteList.push(undefined);
     }
-    if (whiteList.includes(origin)) {
+    if (whiteList.includes(origi)) {
       handler(null, true);
     } else {
       handler(new Error("Not allowed by CORS"));
